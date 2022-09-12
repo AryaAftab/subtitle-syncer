9f6882ed56569d051f171088daf48911087057b0
Arya Aftab <arya.aftab@gmail.com>
2022-09-12 08:16:36 +0430

    Find nearest time for entered statr point

--- subtitle_syncer.py
+++ subtitle_syncer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 
 
@@ -16,43 +17,84 @@ def find_dialogue(string):
     return 'Dialogue' in string.split(',')[0]
 
 
-def start_end(string):
+def find_start_end(string):
     Splited = string.split(',')
     return [float(s) for s in Splited[1].split(':')], [float(s) for s in Splited[2].split(':')]
 
 
-def add(start, end, Delta):
-    new_start = (start[0] * 60 + start[1]) * 60 + start[2] + (Delta[0] * 60 + Delta[1]) * 60 + Delta[2]
-    new_end = (end[0] * 60 + end[1]) * 60 + end[2] + (Delta[0] * 60 + Delta[1]) * 60 + Delta[2]
-
-    new_start, sec_start = divmod(new_start, 60)
-    new_end, sec_end = divmod(new_end, 60)
-
-    hr_start, min_start = divmod(new_start, 60)
-    hr_end, min_end = divmod(new_end, 60)
-
-    return "%d:%02d:%02.2f" % (hr_start, min_start, sec_start), "%d:%02d:%02.2f" % (hr_end, min_end, sec_end)
+def time_to_second(h_m_s):
+    return (h_m_s[0] * 60 + h_m_s[1]) * 60 + h_m_s[2]
 
 
+def second_to_time(second):
+    second, Sec = divmod(second, 60)
+    Hr, Min = divmod(second, 60)
+    return [Hr, Min, round(Sec, 2)]
 
-h, m, s = arguments.start_point
-h, m, s = int(h), int(m), int(s)
-start_point = f"{h%10}:{m:02d}:{s:02d}"
+
+def to_format(h, m, s):
+    return "%d:%02d:%02.2f" % (h, m, s)
 
 
-flag = False
+def add_time(start, end, Delta):
+    new_start = time_to_second(start) + time_to_second(Delta)
+    new_end = time_to_second(end) + time_to_second(Delta)
+
+    hr_start, min_start, sec_start = second_to_time(new_start)
+    hr_end, min_end, sec_end = second_to_time(new_end)
+
+    return to_format(hr_start, min_start, sec_start), to_format(hr_end, min_end, sec_end)
+
+
+def correct_line(line, delay):
+    start, end = find_start_end(line)
+    new_start, new_end = add_time(start, end, delay)
+    new_line = line.split(',')
+    new_line[1] = new_start
+    new_line[2] = new_end
+    new_line = ",".join(new_line)
+
+    return new_line
+
+
+def start_point_correction(arguments):
+
+    start_point = time_to_second(arguments.start_point)
+
+    with open(arguments.input, 'r') as r:
+        all_dialogue = []
+        for line in r.readlines():
+            if find_dialogue(line):
+                start, _ = find_start_end(line)
+                start = time_to_second(start)
+                all_dialogue.append(start)
+
+    for counter in range(len(all_dialogue) - 1):
+        if all_dialogue[counter] > all_dialogue[counter+1]:
+            all_dialogue = all_dialogue[:counter+1]
+            break
+
+    diff_dialogue = [abs(dialogue - start_point) for dialogue in all_dialogue]
+    best_index = min(range(len(diff_dialogue)), key=lambda x : diff_dialogue[x])
+    start_point = second_to_time(all_dialogue[best_index])
+
+    return to_format(*start_point)
+
+
+
+
+
+start_point = start_point_correction(arguments)
+
+
+find_flag = False
 with open(arguments.output + ".ass","w+") as w:
     with open(arguments.input, 'r') as r:
         for line in r.readlines():
-            if (start_point in line) or flag:
+            if (start_point in line) or find_flag:
                 if find_dialogue(line):
-                    start, end = start_end(line)
-                    new_start, new_end = add(start, end, arguments.delay)
-                    new_line = line.split(',')
-                    new_line[1] = new_start
-                    new_line[2] = new_end
-                    new_line = ",".join(new_line)
+                    new_line = correct_line(line, arguments.delay)
                     w.write(new_line)
-                    flag = True
+                    find_flag = True
             else:
                 w.write(line)
